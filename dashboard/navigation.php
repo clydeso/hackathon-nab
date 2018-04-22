3acd20aa433230f94cdbaa556d2f530456328e69
Vivian Genato <vivian@liquorice.com.au>
2018-04-22 13:57:35 +1000

    gettingstarted

--- dashboard/navigation.php
+++ dashboard/navigation.php
@@ -1,21 +1,24 @@
 <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-    <div class="sidebar-sticky"> 
+    <div class="sidebar-sticky">
         <ul class="nav flex-column">
             <li class="nav-item">
+                <a class="nav-link" href="<?php echo FULL_URL ?>dashboard/gettingstarted"><span data-feather="play"></span> <span classs='link__text'>Getting Started</span> <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
                 <a class="nav-link" href="<?php echo FULL_URL ?>dashboard/#"><span data-feather="home"></span> Dashboard <span class="sr-only">(current)</span></a>
-            </li> 
+            </li>
             <li class="nav-item">
                 <a class="nav-link" href="<?php echo FULL_URL ?>dashboard/product"><span data-feather="shopping-cart"></span> Products</a>
-            </li> 
+            </li>
             <li class="nav-item">
                 <a class="nav-link" href="<?php echo FULL_URL ?>dashboard/integration"><span data-feather="layers"></span> Integrations</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="<?php echo FULL_URL ?>dashboard/resources"><span data-feather="file-text"></span> Resources</a>
-            </li> 
+            </li>
             <li class="nav-item">
                 <a class="nav-link" href="<?php echo FULL_URL ?>dashboard/help"><span data-feather="help-circle"></span> Need Help?</a>
-            </li> 
-        </ul> 
+            </li>
+        </ul>
     </div>
 </nav>
