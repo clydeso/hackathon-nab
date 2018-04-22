0cb047471ac1ef2bf3a7a9a8aac621b75941b5e5
Clyde So <clydeso@gmail.com>
2018-04-22 10:38:42 +1000

    Added logos and changed sign out button colour

--- dashboard/header.php
+++ dashboard/header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-dark fixed-top flex-md-nowrap p-0 shadow">
+<nav class="navbar navbar-dark fixed-top flex-md-nowrap p-0 shadow bg-white">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="<?php echo FULL_URL ?>dashboard">
 	    <img class="float-left" src="<?php echo FULL_URL ?>assets/images/nab.png" height="55">
 	</a> 
@@ -6,7 +6,7 @@
     <!-- <input aria-label="Search" class="form-control form-control-dark w-100" placeholder="Search" type="text"> -->
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
-            <button type="button" class="btn btn btn-light" onclick="window.location='<?php echo FULL_URL ?>?logout'">Sign out</button>
+            <button type="button" class="btn btn btn-danger" style="background-color: #be0d00;" onclick="window.location='<?php echo FULL_URL ?>?logout'">Sign out</button>
         </li>
     </ul>
 </nav>
